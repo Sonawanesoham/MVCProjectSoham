edd87f0cf5c34c643f04e325be5483bf7cedea96
Sonawanesoham2002 <sonawanesoham2002@gmail.com>
2024-07-18 15:01:14 +0530

    Add project files.

---
+++ DBContext/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCProjectSoham.Models;
+namespace MVCProjectSoham.DBContext
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) :base(options)
+        {
+
+        }
+
+        public  DbSet<Designation> Designation { get; set; }
+        public DbSet<DesignationGrade> DesignationGrade {  get; set; }
+        public DbSet <Employee> Employee {  get; set; }
+
+    }
+}
